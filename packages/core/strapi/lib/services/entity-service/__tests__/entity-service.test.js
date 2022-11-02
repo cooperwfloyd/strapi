b706c19d8f528ce78d1ff4e9abf473a61cf3b5d7
nathan-pichon <nathan.pichon@strapi.io>
2022-11-02 15:35:27 +0100

    test(entity-service): add create tests using upload files

--- packages/core/strapi/lib/services/entity-service/__tests__/entity-service.test.js
+++ packages/core/strapi/lib/services/entity-service/__tests__/entity-service.test.js
@@ -6,6 +6,8 @@ const { EventEmitter } = require('events');
 const createEntityService = require('..');
 const entityValidator = require('../../entity-validator');
 
+jest.mock('../../utils/upload-files', () => jest.fn(() => Promise.resolve()));
+
 describe('Entity service', () => {
   global.strapi = {
     getModel: jest.fn(() => ({})),
@@ -14,6 +16,7 @@ describe('Entity service', () => {
         return [];
       },
     },
+    query: jest.fn(() => ({})),
   };
 
   describe('Decorator', () => {
@@ -79,16 +82,46 @@ describe('Entity service', () => {
   });
 
   describe('Create', () => {
+    let instance;
+    const fakeQuery = {
+      count: jest.fn(() => 0),
+      create: jest.fn(({ data }) => ({
+        id: 1,
+        ...data,
+      })),
+      findOne: jest.fn(),
+    };
+    const fakeModels = {};
+
+    beforeAll(() => {
+      global.strapi.getModel.mockImplementation((modelName) => fakeModels[modelName]);
+      global.strapi.query.mockImplementation(() => fakeQuery);
+
+      const fakeDB = {
+        query: jest.fn(() => fakeQuery),
+      };
+
+      const fakeStrapi = {
+        getModel: jest.fn((modelName) => fakeModels[modelName]),
+      };
+
+      instance = createEntityService({
+        strapi: fakeStrapi,
+        db: fakeDB,
+        eventHub: new EventEmitter(),
+        entityValidator,
+      });
+    });
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+    afterAll(() => {
+      global.strapi.getModel.mockImplementation(() => ({}));
+    });
+
     describe('assign default values', () => {
-      let instance;
-
       beforeAll(() => {
-        const fakeQuery = {
-          count: jest.fn(() => 0),
-          create: jest.fn(({ data }) => data),
-        };
-
-        const fakeModel = {
+        fakeModels['test-model'] = {
           kind: 'contentType',
           modelName: 'test-model',
           privateAttributes: [],
@@ -114,23 +147,7 @@ describe('Entity service', () => {
             attrPassword: { type: 'password' },
           },
         };
-
-        const fakeDB = {
-          query: jest.fn(() => fakeQuery),
-        };
-
-        const fakeStrapi = {
-          getModel: jest.fn(() => fakeModel),
-        };
-
-        instance = createEntityService({
-          strapi: fakeStrapi,
-          db: fakeDB,
-          eventHub: new EventEmitter(),
-          entityValidator,
-        });
       });
-
       test('should create record with all default attributes', async () => {
         const data = {};
 
@@ -183,6 +200,94 @@ describe('Entity service', () => {
           ...data,
           attrPassword: 'secret-password',
         });
+      });
+    });
+
+    describe('with files', () => {
+      beforeAll(() => {
+        fakeModels['test-model'] = {
+          kind: 'collectionType',
+          collectionName: 'test-model',
+          options: {},
+          attributes: {
+            name: {
+              type: 'string',
+            },
+            activity: {
+              displayName: 'activity',
+              type: 'component',
+              repeatable: true,
+              component: 'basic.activity',
+            },
+          },
+          modelType: 'contentType',
+          modelName: 'test-model',
+        };
+        fakeModels['basic.activity'] = {
+          collectionName: 'components_basic_activities',
+          info: {
+            displayName: 'activity',
+          },
+          options: {},
+          attributes: {
+            docs: {
+              allowedTypes: ['images', 'files', 'videos', 'audios'],
+              type: 'media',
+              multiple: true,
+            },
+            name: {
+              type: 'string',
+            },
+          },
+          uid: 'basic.activity',
+          category: 'basic',
+          modelType: 'component',
+          modelName: 'activity',
+          globalId: 'ComponentBasicActivity',
+        };
+      });
+      test('should create record with attached files', async () => {
+        const uploadFiles = require('../../utils/upload-files');
+        global.strapi.getModel.mockImplementation((modelName) => fakeModels[modelName]);
+        const data = {
+          name: 'demoEvent',
+          activity: [{ name: 'Powering the Aviation of the Future' }],
+        };
+        const files = {
+          'activity.0.docs': {
+            size: 381924,
+            path: '/tmp/upload_4cab76a3a443b584a1fd3aa52e045130',
+            name: 'thisisajpeg.jpeg',
+            type: 'image/jpeg',
+            mtime: '2022-11-03T13:36:51.764Z',
+          },
+        };
+
+        fakeQuery.findOne.mockResolvedValue({ id: 1, ...data });
+
+        await instance.create('test-model', { data, files });
+
+        expect(global.strapi.getModel).toBeCalled();
+        expect(uploadFiles).toBeCalled();
+        expect(uploadFiles).toBeCalledTimes(1);
+        expect(uploadFiles).toBeCalledWith(
+          'test-model',
+          {
+            id: 1,
+            name: 'demoEvent',
+            activity: [
+              {
+                id: 1,
+                __pivot: {
+                  order: expect.any(Number),
+                  field: 'activity',
+                  component_type: 'basic.activity',
+                },
+              },
+            ],
+          },
+          files
+        );
       });
     });
   });
